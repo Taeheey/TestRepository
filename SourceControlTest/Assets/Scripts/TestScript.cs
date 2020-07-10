836513ceaaa173061c798499530b1e6242317d62
Taeheey <67844649+Taeheey@users.noreply.github.com>
2020-07-10 14:16:00 -0700

    commit

--- SourceControlTest/Assets/Scripts/TestScript.cs
+++ SourceControlTest/Assets/Scripts/TestScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        start void update 
     }
 
     // Update is called once per frame
